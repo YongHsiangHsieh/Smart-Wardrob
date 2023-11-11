ee48a2b3255208136ce565b4a3b244c20fe882fe
Wilson <wilsonhsieh1216@gmail.com>
2023-11-11 11:50:53 +0000

    Test JsonSerializer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,18 @@ import utils.JsonUtils
 import model.*
 
 fun main(args: Array<String>) {
-    val cloths = Clothing(1, model.ClothingType.JUMPER, "Nike", "Nike Jumper", "Black", "Cotton")
+    val cloth1 = Clothing(1, model.ClothingType.JUMPER, "Nike", "Nike Jumper", "Black", "Cotton")
+    val cloth2 = Clothing(2, model.ClothingType.SHIRT, "Adidas", "Adidas Shirt", "White", "Cotton")
+    val cloth3 = Clothing(3, model.ClothingType.SHORTS, "Puma", "Puma Shorts", "Blue", "Cotton")
+    val cloth4 = Clothing(4, model.ClothingType.TRACKSUIT, "Reebok", "Reebok Tracksuit", "Red", "Cotton")
+    val cloths = listOf(cloth1, cloth2, cloth3, cloth4)
+    for (cloth in cloths) {
+        println(cloth)
+    }
     val jsonCloths = JsonUtils.serializeToJson(cloths)
-    println(cloths)
     println(jsonCloths)
+    val clothsFromJson = JsonUtils.deserializeFromJson<List<Clothing>>(jsonCloths)
+    for (cloth in clothsFromJson) {
+        println(cloth)
+    }
 }
