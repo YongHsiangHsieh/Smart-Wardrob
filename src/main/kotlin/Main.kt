8b551e3487ed9b86d52ce5f7b75c563f11031a4c
Wilson <wilsonhsieh1216@gmail.com>
2023-11-13 11:14:35 +0000

    Add init to make sure directories exist and saveUserData, loadUserData in PersistenceManager.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,29 @@
 import utils.JsonUtils
+import persistence.PersistenceManager
 import model.*
 
 fun main(args: Array<String>) {
     val cloth1 = Clothing(1, ClothingType.JUMPER, "Nike", "Nike Jumper", "Red", "Cotton")
-    println(cloth1)
+    val cloth2 = Clothing(2, ClothingType.SHIRT, "Adidas", "Adidas Shirt", "Blue", "Cotton")
+    val cloth3 = Clothing(3, ClothingType.SHORTS, "Puma", "Puma Shorts", "Black", "Cotton")
+//    println(cloth1)
 
     val user1 = User("user1", "abc")
     user1.wardrobe.addClothing(cloth1)
-    println(user1.toString())
+    user1.wardrobe.addClothing(cloth2)
+    user1.wardrobe.addClothing(cloth3)
+//    println(user1.toString())
 
-    val wardrobe1 = Wardrobe()
-    wardrobe1.addClothing(cloth1)
-    println(wardrobe1)
+//    val wardrobe1 = Wardrobe()
+//    wardrobe1.addClothing(cloth1)
+//    println(wardrobe1)
+
+    val user1Json = JsonUtils.serializeToJson(user1)
+    println(user1Json)
+
+    val user1Obj: User = JsonUtils.deserializeFromJson(user1Json)
+    println(user1Obj)
+
+
 
 }
