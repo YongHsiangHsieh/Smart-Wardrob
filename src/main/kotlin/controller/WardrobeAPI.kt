1e53b8a5bb6699480175ba1eb33f7772fdf2398a
Wilson <wilsonhsieh1216@gmail.com>
2023-11-16 13:32:14 +0000

    Update WardrobeAPI to have default value of null in the constructor

--- src/main/kotlin/controller/WardrobeAPI.kt
+++ src/main/kotlin/controller/WardrobeAPI.kt
@@ -4,39 +4,39 @@ import model.Clothing
 import model.ClothingType
 import model.Wardrobe
 
-class WardrobeAPI(private val wardrobe: Wardrobe) {
+class WardrobeAPI(private var wardrobe: Wardrobe? = null) {
 
     fun addClothingToWardrobe(clothingData: Map<String, String>): Boolean {
         val id = clothingData["id"]?.toIntOrNull() ?: return false
         val type = clothingData["type"]?.let { ClothingType.valueOf(it) } ?: return false
         val color = clothingData["color"] ?: return false
         val texture = clothingData["texture"] ?: return false
-        return wardrobe.addClothing(Clothing(id, type, color, texture))
+        return wardrobe?.addClothing(Clothing(id, type, color, texture)) ?: false
     }
 
     fun updateClothingInWardrobe(clothingId: Int, clothingData: Map<String, String>): Boolean {
         val color = clothingData["color"]?: return false
         val texture = clothingData["texture"]?: return false
-        return wardrobe.updateClothing(clothingId, color, texture)
+        return wardrobe?.updateClothing(clothingId, color, texture) ?: false
     }
 
     fun deleteClothingFromWardrobe(clothingId: Int): Boolean {
-        return wardrobe.deleteClothing(clothingId)
+        return wardrobe?.deleteClothing(clothingId) ?:  false
     }
 
     fun getAllClothing(): List<Clothing> {
-        return wardrobe.getAllClothing()
+        return wardrobe?.getAllClothing() ?: emptyList()
     }
 
     fun getClothingById(id: Int): Clothing? {
-        return wardrobe.getClothingById(id)
+        return wardrobe?.getClothingById(id)
     }
 
     fun getClothingByType(type: ClothingType): List<Clothing> {
-        return wardrobe.getClothesByType(type)
+        return wardrobe?.getClothesByType(type) ?: emptyList()
     }
 
     fun searchClothingByColorAndType(color: String, type: ClothingType): List<Clothing> {
-        return wardrobe.searchByColorAndType(color, type)
+        return wardrobe?.searchByColorAndType(color, type) ?: emptyList()
     }
 }
