864c73cf25f7d1c1fedfdd2853557da14a5815e8
Wilson <wilsonhsieh1216@gmail.com>
2023-11-23 10:27:50 +0000

    Add in dotenv-kotlin repo and dependency in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
     implementation("org.slf4j:slf4j-simple:2.0.9")
     implementation("io.github.microutils:kotlin-logging:3.0.5")
-    implementation("io.github.cdimascio:dotenv-java:5.2.2")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
 //    implementation("com.squareup.okhttp3:okhttp:4.9.0")
 
 //    implementation("org.yaml:snakeyaml:1.33")
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,15 @@ import controller.UserAPI
 import controller.WardrobeAPI
 import persistence.PersistenceManager
 import view.ConsoleView
+import io.github.cdimascio.dotenv.dotenv
+
 
 fun main(args: Array<String>) {
-    val userAPI = UserAPI(PersistenceManager)
-    val wardrobeAPI = WardrobeAPI()
-    val consoleView = ConsoleView(userAPI, wardrobeAPI)
+//    val userAPI = UserAPI(PersistenceManager)
+//    val wardrobeAPI = WardrobeAPI()
+//    val consoleView = ConsoleView(userAPI, wardrobeAPI)
+//    consoleView.startApplication()
 
-    consoleView.startApplication()
+    val dotenv = dotenv()
 
 }
