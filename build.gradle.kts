35d58ba884852caebe565d0d0989c58788d40e7d
Wilson <wilsonhsieh1216@gmail.com>
2023-11-29 09:41:12 +0000

    Add Dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.20"
+    // Plugin for Dokka - KDoc generating tool
+    id("org.jetbrains.dokka") version "1.9.10"
     application
     kotlin("plugin.serialization") version "1.5.0"
 }
