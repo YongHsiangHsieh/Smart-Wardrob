e0d526339d76c2527c462ed02767e23d63070308
Wilson <wilsonhsieh1216@gmail.com>
2023-11-29 22:06:18 +0000

    Add WardrobeManager.kt in the service package to work between WardrobeAPI and view

---
+++ src/main/kotlin/service/WardrobeManager.kt
@@ -0,0 +1,40 @@
+package service
+
+import controller.WardrobeAPI
+import model.Clothing
+import model.ClothingType
+
+
+class WardrobeManager (private val wardrobeAPI: WardrobeAPI){
+    fun addClothing(id: Int, type: ClothingType, brand: String, name: String, color: String, texture: String): Boolean {
+        val clothingData = mapOf(
+            "id" to id.toString(),
+            "type" to type.toString(),
+            "brand" to brand,
+            "name" to name,
+            "color" to color,
+            "texture" to texture
+        )
+        return wardrobeAPI.addClothingToWardrobe(clothingData)
+    }
+
+    fun updateClothing(color: String, texture: String, id: Int): Boolean {
+        val clothingData = mapOf(
+            "color" to color,
+            "texture" to texture
+        )
+        return wardrobeAPI.updateClothingInWardrobe(id, clothingData)
+    }
+
+    fun removeClothing(id: Int) = wardrobeAPI.deleteClothingFromWardrobe(id)
+
+    fun getAllClothing(): List<Clothing> =  wardrobeAPI.getAllClothing()
+
+    fun getClothingByType(type: ClothingType): List<Clothing> = wardrobeAPI.getClothingByType(type)
+
+    fun getClothingByTypeAndColor(type: ClothingType, color: String): List<Clothing> = wardrobeAPI.searchClothingByColorAndType(color, type)
+
+
+
+
+}
