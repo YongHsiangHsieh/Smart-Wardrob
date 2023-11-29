0a6449b8579fe9959d7625783b123d888424481a
Wilson <wilsonhsieh1216@gmail.com>
2023-11-29 21:18:30 +0000

    Update UserManager.kt

--- src/main/kotlin/service/UserManager.kt
+++ src/main/kotlin/service/UserManager.kt
@@ -4,24 +4,10 @@ import controller.UserAPI
 import model.User
 import utils.LoggerUtil.printLogger
 
-class UserManager (private val userAPI: UserAPI) {
-    fun createUser(username: String, password: String) {
-        val success = userAPI.createUser(username, password)
-        if (success) {
-            printLogger("User created successfully")
-        } else {
-            printLogger("User already exists")
-        }
-    }
+class UserManager(private val userAPI: UserAPI) {
+    fun createUser(username: String, password: String): Boolean = userAPI.createUser(username, password)
 
-     fun deleteUser(username: String) {
-        val success = userAPI.deleteUser(username)
-        if (success) {
-            printLogger("User deleted successfully")
-        } else {
-            printLogger("User does not exist")
-        }
-    }
+    fun deleteUser(username: String): Boolean = userAPI.deleteUser(username)
 
     fun userLogin(username: String, password: String): User? {
         return userAPI.findUser(username)?.takeIf { userAPI.authenticateUser(it, password) }
