d8cf2d8c72ea838daf2a41bdc3554c3dbb6f0f7b
Wilson <wilsonhsieh1216@gmail.com>
2023-11-29 10:38:14 +0000

    Replace the wildcard import

--- src/test/kotlin/controller/UserAPITest.kt
+++ src/test/kotlin/controller/UserAPITest.kt
@@ -2,7 +2,11 @@ package controller
 
 import model.ClothingType
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import persistence.PersistenceManager
