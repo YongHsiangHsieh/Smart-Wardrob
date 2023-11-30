a72a0f9337c81c40148d40c13411bff0a9fda48c
Wilson <wilsonhsieh1216@gmail.com>
2023-11-30 13:07:55 +0000

    Write Kdoc for toString in Clothing.kt

--- src/main/kotlin/model/Clothing.kt
+++ src/main/kotlin/model/Clothing.kt
@@ -36,6 +36,12 @@ data class Clothing(
     var color: String = "Unknown",
     var texture: String = "Unknown"
 ) {
+    /**
+     * Generates a string representation of the clothing item.
+     * It includes all the attributes in a readable format.
+     *
+     * @return A string describing the clothing item details.
+     */
     override fun toString(): String =
         "Clothing Details:\n" + "ID: $id\n" + "Type: $type\n" + "Brand: $brand\n" + "Name: $name\n" +
                 "Color: $color\n" + "Texture: $texture\n"
