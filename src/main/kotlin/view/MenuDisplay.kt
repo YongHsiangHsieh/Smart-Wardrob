69e124ed86d2b95338d52a42182aeb9053585398
Wilson <wilsonhsieh1216@gmail.com>
2023-11-30 09:54:11 +0000

    Update MenuDisplay to have simple validations checking

--- src/main/kotlin/view/MenuDisplay.kt
+++ src/main/kotlin/view/MenuDisplay.kt
@@ -3,72 +3,113 @@ package view
 import utils.ScannerInput
 
 object MenuDisplay {
-    fun displayMainMenu(): Int =
-        ScannerInput.readNextInt(
-            """
-            1 -> Admin
-            2 -> Smart Wardrobe
-            0 -> Exit
-            Enter option: 
-            """.trimIndent()
-        )
+    fun displayMainMenu(): Int {
+        while (true) {
+            val choice = ScannerInput.readNextInt(
+                """
+                1 -> Admin
+                2 -> Smart Wardrobe
+                0 -> Exit
+                Enter option: 
+                """.trimIndent()
+            )
+            if (choice in 0..2) {
+                return choice
+            }
+            println("Invalid option, please try again")
+        }
+    }
 
-    fun displayUserManagementMenu(): Int =
-        ScannerInput.readNextInt(
-            """
-            User Management:
-            1 -> Create User
-            2 -> Delete User
-            0 -> Back to Main Menu
-            Enter option: 
-            """.trimIndent()
-        )
+    fun displayUserManagementMenu(): Int {
+        while (true) {
+            val choice = ScannerInput.readNextInt(
+                """
+                1 -> Create User
+                2 -> Delete User
+                0 -> Back to Main Menu
+                Enter option: 
+                """.trimIndent()
+            )
+            if (choice in 0..2) {
+                return choice
+            }
+            println("Invalid option, please try again")
+        }
+    }
 
-    fun displayWardrobe(): Int =
-        ScannerInput.readNextInt(
-            """
+    fun displayWardrobe(): Int {
+        while (true) {
+            val choice = ScannerInput.readNextInt(
+                """
             1 -> Today's Outfit
             2 -> View Wardrobe
             3 -> Manage Wardrobe
             0 -> Back to Main Menu
             Enter option: 
             """.trimIndent()
-        )
+            )
+            if (choice in 0..3) {
+                return choice
+            }
+            println("Invalid option, please try again")
+        }
+    }
 
-    fun displayManageWardrobe(): Int =
-        ScannerInput.readNextInt(
-            """
-            Wardrobe Management:
+    fun displayManageWardrobe(): Int {
+        while (true) {
+            val choice = ScannerInput.readNextInt(
+                """
             1 -> Add Clothing
             2 -> Update Clothing
             3 -> Remove Clothing
             0 -> Back to Wardrobe Menu
             Enter option: 
             """.trimIndent()
-        )
+            )
+            if (choice in 0..3) {
+                return choice
+            }
+            println("Invalid option, please try again")
+        }
+    }
 
-    fun displayViewWardrobe(): Int =
-        ScannerInput.readNextInt(
-            """
-            Wardrobe View:
+    fun displayViewWardrobe(): Int {
+        while (true) {
+            val choice = ScannerInput.readNextInt(
+                """
             1 -> View All Clothing
             2 -> View Clothing by Type
             3 -> View Clothing by Type and Color
             0 -> Back to Wardrobe Menu
             Enter option: 
             """.trimIndent()
-        )
+            )
+            if (choice in 0..3) {
+                return choice
+            }
+            println("Invalid option, please try again")
+        }
+    }
 
-    fun displayClothingType(): Int =
-        ScannerInput.readNextInt(
-            """
+    fun displayClothingType(): Int {
+        while (true) {
+            val choice = ScannerInput.readNextInt(
+                """
             Clothing Types:
             1 -> JUMPER
             2 -> SHIRT
             3 -> SHORTS
             4 -> TRACKSUIT
             5 -> JACKET
+            0 -> Back to Wardrobe Menu
             Enter option: 
-                """.trimIndent()
-        )
+            """.trimIndent()
+            )
+            if (choice in 0..5) {
+                return choice
+            }
+            println("Invalid option, please try again")
+        }
+    }
+
 }
