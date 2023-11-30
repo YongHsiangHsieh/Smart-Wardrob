9ac129df0e5396e62ecb265f1284c61cd92e3fc1
Wilson <wilsonhsieh1216@gmail.com>
2023-11-30 17:54:53 +0000

    Add getApiResponse and fields in WeatherAPI to get data from open weather api

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,18 @@
 import controller.UserAPI
 import controller.WardrobeAPI
+import controller.WeatherAPI
+import io.github.cdimascio.dotenv.dotenv
 import persistence.PersistenceManager
 import view.ConsoleView
 
 fun main() {
-    val userAPI = UserAPI(PersistenceManager)
-    val wardrobeAPI = WardrobeAPI()
-    val consoleView = ConsoleView(userAPI, wardrobeAPI)
-    consoleView.startApplication()
+//    val userAPI = UserAPI(PersistenceManager)
+//    val wardrobeAPI = WardrobeAPI()
+//    val consoleView = ConsoleView(userAPI, wardrobeAPI)
+//    consoleView.startApplication()
 
-//    val dotenv = dotenv()
+    val response = WeatherAPI.getApiResponse()
+    println(response)
+
+
 }
--- src/main/kotlin/controller/WeatherAPI.kt
+++ src/main/kotlin/controller/WeatherAPI.kt
@@ -1,4 +1,36 @@
 package controller
 
-class WeatherAPI {
+import io.github.cdimascio.dotenv.dotenv
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+object WeatherAPI {
+    private val dotenv = dotenv()
+    private const val BASE_URL = "https://api.openweathermap.org/data/3.0/onecall"
+    private val parameters = mapOf(
+        "lat" to "52.259319",
+        "lon" to "-7.110070",
+        "appid" to dotenv["OPEN_WEATHER_API_KEY"],
+        "exclude" to "current,minutely,daily"
+    )
+    fun getApiResponse(): String {
+        val client = OkHttpClient()
+
+        val httpUrlBuilder = BASE_URL.toHttpUrlOrNull()?.newBuilder() ?: throw IllegalArgumentException("Invalid URL")
+        parameters.forEach { (key, value) ->
+            httpUrlBuilder.addQueryParameter(key, value)
+        }
+        val url = httpUrlBuilder.build()
+
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            return response.body?.string() ?: ""
+        }
+    }
 }
