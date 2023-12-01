660a3306640a43b209cd52657d5770b45bb1cc9d
Wilson <wilsonhsieh1216@gmail.com>
2023-12-01 13:59:10 +0000

    Update

--- src/test/kotlin/service/WardrobeManagerTest.kt
+++ src/test/kotlin/service/WardrobeManagerTest.kt
@@ -6,8 +6,8 @@ import model.Wardrobe
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertNull
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Assertions.assertSame
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
