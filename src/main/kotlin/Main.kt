0725162afd605869fad6bf403ca6b06751191c1f
Wilson <wilsonhsieh1216@gmail.com>
2023-12-01 14:44:40 +0000

    Write Kdoc for YamlUtil.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,13 @@
 import controller.UserAPI
 import controller.WardrobeAPI
-import model.Clothing
-import model.ClothingType
-import model.Wardrobe
 import persistence.PersistenceManager
-import utils.YamlUtil
 import view.ConsoleView
 
 fun main() {
-//    val userAPI = UserAPI(PersistenceManager)
-//    val wardrobeAPI = WardrobeAPI()
-//    val consoleView = ConsoleView(userAPI, wardrobeAPI)
-//    consoleView.startApplication()
+    val userAPI = UserAPI(PersistenceManager)
+    val wardrobeAPI = WardrobeAPI()
+    val consoleView = ConsoleView(userAPI, wardrobeAPI)
+    consoleView.startApplication()
 
 
 }
